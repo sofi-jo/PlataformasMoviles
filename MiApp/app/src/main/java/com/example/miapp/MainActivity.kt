7148ddfd28df42d614d3873fb13bc37b864fd1fc
Sofia Jaramillo <asjaramillo9@utpl.edu.ec>
2022-10-27 21:13:17 -0500

    Actualizacion

--- MiApp/app/src/main/java/com/example/miapp/MainActivity.kt
+++ MiApp/app/src/main/java/com/example/miapp/MainActivity.kt
@@ -8,10 +8,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         edadPersonas()
-        multiplicacionAsceYDes()
         listadoEstudiantes()
         calculoIva()
-        
+        validacionCedula()
+
     }
 
     fun edadPersonas(){
@@ -25,9 +25,13 @@ class MainActivity : AppCompatActivity() {
     fun multiplicacionAsceYDes(){
         var num = 8
         var res = 0
-        for (num in i..10)
-            res = num * i
-            println(res)
+        for (a in 0..10)
+            res = a * num
+            println("$a * $num = $res")
+
+        for (d in 0 downTo 10)
+            res = d * num
+            println("$d * $num = $res")
     }
 
     fun listadoEstudiantes(){
@@ -42,8 +46,19 @@ class MainActivity : AppCompatActivity() {
             "Pablo Jose Sanchez Saraguro", "Jose Isaias Silva Leon", "Carlos Andres Tandazo Malla"
         )
 
-        for (lista in l)
-            println(lista[l])
+        println("Lista de estudiantes Plataformas Moviles")
+        for (l in lista) {
+            println(l)
+        }
+
+        println("Subgrupos por proyectos")
+        println("Proyecto 1")
+        println("Grupo 1: ${lista.get(0)},${lista.get(11)}, ${lista.get(2)}")
+        println("Grupo 2: ${lista.get(1)},${lista.get(6)}, ${lista.get(7)}")
+        println("Grupo 3: ${lista.get(13)},${lista.get(14)}, ${lista.get(15)}")
+        println("Proyecto 2")
+        println("Grupo 4: ${lista.get(9)},${lista.get(12)}, ${lista.get(10)}")
+
     }
 
     fun propiedadesVehiculo(){
@@ -55,22 +70,26 @@ class MainActivity : AppCompatActivity() {
         var precio = 12.5
         var incremento = precio * iva
         var total = precio + incremento
-        println(total)
+        println("El total precio total con iva es: $total")
     }
 
     fun validacionCedula(){
         val numCedula = listOf<Int>(1,1,5,0,5,6,3,9,8,7)
         val numAlgo = listOf<Int>(2,1,2,1,2,1,2,1,2)
-        var sum = 0
-        var mul = 0
         var acum = 0
-        for (numAlgo in n)
+        var total = 0
+        for (n in numAlgo){
+            var mul = 0
             mul = numAlgo[n] * numCedula[n]
-            if ( mul < 10)
+            if (mul < 10)
                 acum = acum + mul
             else
-                sum =
-
+                mul = mul - 9
+                acum = acum + mul
+        }
+        total = 40 - acum
+        if (total == numCedula[0])
+            println("Es un numero de cedula correcto")
     }
 
 }
